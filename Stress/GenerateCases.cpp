0a5ffb6b9c54384dc51ee28d132566ca534c075f
Akshat madhavan <112942320+GokuUu007@users.noreply.github.com>
2024-06-12 23:45:18 +0530

    Update GenerateCases.cpp

--- Stress/GenerateCases.cpp
+++ Stress/GenerateCases.cpp
@@ -1,19 +1,56 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
+
+// l - r | generates numbers in range from [l , r) inclusive
+template<typename T> 
+T generate_number(T l, T r) {
+  return l + (rand() % r);
+}
+
+// [l - r) -> range of values for the array
+// n -> size of array
+template<typename T>
+void generate_array(T l, T r, T n) {
+  for (int i = 0; i < n; ++i) {
+    cout << generate_number(l, r) << " ";
+  }
+  cout << "\n";
+}
+
+template<typename T>
+void generate_permutation(T l, T r) {
+  vector<int> perm(r - l + 1);
+  iota(perm.begin(), perm.end(), l);
+  random_shuffle(perm.begin(), perm.end());
+  
+  for (auto &x : perm)
+    cout << x << " ";
+  
+  cout << "\n"; 
+}
+
+// 0 <= l <= r <= 26 -> 0-based indexing
+void generate_string(int l, int r, int size) {
+  for (int i = 0; i < size; ++i) {
+    cout << char('a' + generate_number(l, r));
+  }
+  cout << "\n";
+}
 
 int main (int argc, char **argv) {
   // argv is an array of strings
   // atoi is a C function for converting a string into an int
 
-  srand(atoi(argv[1])); // srand sets the random seed
-  int lim = atoi(argv[2]);
-  int lim2 = atoi(argv[3]);
-  int  x = 1 + (rand() % lim);
-  int  y = 1 + (rand() % lim);
-  int  z = 1 + (rand() % lim);
-  long long int k = 1 + (rand() % lim2);
-  cout<<"1\n";
-  cout<<x<<" "<<y<<" "<<z<<" ";
-  cout<<k<<"\n";
+  srand(time(0)); // srand sets the random seed
+
+  // pass the range of number of testcases you want
+
+  int t = generate_number(1,10);
+  cout << t << "\n";
+  while (t--) {
+    /*
+       generate whatever you wanna generate down here - >
+    */
+  }
 }
